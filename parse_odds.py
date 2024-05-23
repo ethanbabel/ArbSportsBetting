6203b0f3a5c2e6a8480891be18c605c00a429301
ethanbabel <babelethan@gmail.com>
2024-05-23 02:03:25 +0000

    added evnet odds scraping

--- parse_odds.py
+++ parse_odds.py
@@ -47,10 +47,37 @@ def get_event_ids(sport: str):
         return event_ids
 
 
+def get_event_odds(sport:str, eventId: str) -> dict:
+    http_request = f'https://api.the-odds-api.com/v4/sports/{sport}/events/{eventId}/odds?apiKey={API_KEY}&regions={REGIONS}&markets={MARKETS}&dateFormat={DATE_FORMAT}&oddsFormat={ODDS_FORMAT}'
+    odds_response = requests.get(http_request)
+
+    if odds_response.status_code != 200:
+        print(f'Failed to get sports: status_code {odds_response.status_code}, response body {odds_response.text}')
+    else:
+        prices = []
+        points = []
+        for bookmaker in odds_response.json()['bookmakers']:
+            outcomes = bookmaker['markets'][0]['outcomes']
+            for outcome in outcomes:
+                price = outcome['price']
+                point = outcome['point']
+                prices.append(price)
+                points.append(point)
+                # print(f'Price:{price}')
+                # print(f'Point:{point}')
+                # print()
+    return {'prices': prices, 'points': points}
+
 if __name__ == "__main__":
+    # for sport in get_in_season_sports():
+    #     for event in get_event_ids(sport):
+    #         get_event_odds(sport, event)
+    get_event_odds('americanfootball_ncaaf', '329ee4daba3514dce8018d5768e5043f')
+
+def compile_event_ids() -> list:
     ids = []
     for sport in get_in_season_sports():
         for id in get_event_ids(sport):
             ids.append(id)
 
-    print(ids)
+    return ids
